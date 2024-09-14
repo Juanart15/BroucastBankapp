b0df8e9c35b268802b4ba008825cc4908a3025b4
Juanart15 <juanpoma@unisabana.edu.co>
2024-09-14 18:25:57 -0500

    Add spring and react

---
+++ backend/broucast_bank/src/main/java/com/broucast_bank/BroucastBankApplication.java
@@ -0,0 +1,13 @@
+package com.broucast_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroucastBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BroucastBankApplication.class, args);
+	}
+
+}
